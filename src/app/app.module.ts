e8432322687fa9ed641b327397fcc21284a53882
KhoaNA <khoa.na1266@sinhvien.hoasen.edu.vn>
2022-08-14 19:16:50 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NoteEffects } from './effects/note.effect';
+import { noteReducer } from './reducers/note.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      note: noteReducer
+    }, {}),
+    EffectsModule.forRoot([NoteEffects]),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
